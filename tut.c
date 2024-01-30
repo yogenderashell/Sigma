ec239216a15ba2bbfc07ace3a76f4598581a0101
yogenderashell <yogendersharmajjj@gmail.com>
2024-01-30 19:57:12 +0530

    sum all the digits of a no.

--- tut.c
+++ tut.c
@@ -1,13 +1,30 @@
-//the code to find the first and last digits of a no. 
+// #include <stdio.h>
+
+// int main()
+// {
+//     // to find the sum of first and the last digit of a no.
+//     int first,last,num =1234;
+//     last = num%10;
+//     while(num>=10){
+//         num/=10;
+//     }
+//     first = num;
+//     printf("the sum of the first and the last digit is : %d",(first+last));
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int first,last,num =1234;
-    last = num%10;
-    while(num>=100){//the no. of digits you want == (100).count(0)
+    //find the sum of all the digits
+    int num = 1234,sum;
+    while (num!=0)
+    {
+        int rem = num%10;
+        sum+=rem;
         num/=10;
     }
-    printf("%d and %d is the first and the last digit of the nummber: ",num,last);
+    printf("the sum of all the digits of a no. is: %d",sum);
     return 0;
 }
